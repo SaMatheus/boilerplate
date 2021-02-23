9b99884ed1755c38adcacbc50c344f48a6539345
matheus <matheus.sa.alves@outlook.com>
2021-02-23 18:28:18 -0300

    Integrando styled-components ao jest

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,7 +1,20 @@
+import * as S from "./styles";
+
 const Main = () => (
-  <main>
-    <h1>React Avançado</h1>
-  </main>
+  <S.Wrapper>
+    <S.Logo
+      src="/img/logo.svg"
+      alt="Imagem de um atomo branco ocm o fundo rosa e um text: React Avançado"
+    />
+    <S.Title>React Avançado</S.Title>
+    <S.Description>
+      Typescript, React.js, Next.js e Styled COmponents
+    </S.Description>
+    <S.Illustration
+      src="/img/hero-illustration.svg"
+      alt="Um desenvolvedor em frente a um computador com códigos"
+    />
+  </S.Wrapper>
 );
 
 export default Main;
