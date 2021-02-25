4e62083644ace8b0602458a919fd38533f6c9307
matheus <matheus.sa.alves@outlook.com>
2021-02-25 15:38:11 -0300

    Acrescentando storybook

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,15 +1,16 @@
 import * as S from "./styles";
 
-const Main = () => (
+const Main = ({
+  title = "React Avançado",
+  description = "Typescript, React.js, Next.js e Styled COmponents",
+}) => (
   <S.Wrapper>
     <S.Logo
       src="/img/logo.svg"
       alt="Imagem de um atomo branco ocm o fundo rosa e um text: React Avançado"
     />
-    <S.Title>React Avançado</S.Title>
-    <S.Description>
-      Typescript, React.js, Next.js e Styled COmponents
-    </S.Description>
+    <S.Title>{title}</S.Title>
+    <S.Description>{description}</S.Description>
     <S.Illustration
       src="/img/hero-illustration.svg"
       alt="Um desenvolvedor em frente a um computador com códigos"
